32f77b278203847c6464d8307050c7810b00c2f5
aditya8755590 <aditya.kumar875590@gmail.com>
2025-07-20 13:57:30 +0530

    fist question

--- week-1/minInSortRotatedArray.java
+++ week-1/minInSortRotatedArray.java
@@ -1,29 +1,23 @@
 
-
 public class minInSortRotatedArray {
-    public static int findMin(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1; 
+    public  static int findMin(int[] nums) {
+        int n=nums.length;
+        int i=0,j=n-1;
+        while(i<j&&i>=0&&j<n){
+            int mid=i+(j-i)/2;
+           if (nums[mid]>nums[j]){
+            i=mid+1;
+           }
+           else{
+            j=mid;
+           }
         }
-        
-        int left = 0, right = nums.length - 1;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-           
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else {
-                right = mid; 
-            }
-        }
-        
-        return nums[left]; 
+        return nums[i];
     }
-    
+
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
         System.out.println("Minimum in rotated sorted array: " + findMin(nums));
     }
+
 }
