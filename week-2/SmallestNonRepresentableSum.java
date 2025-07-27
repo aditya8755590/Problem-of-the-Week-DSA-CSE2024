ff1eec61aac0ec41ee9ca96039b54f5c3d5d8803
aditya8755590 <aditya.kumar875590@gmail.com>
2025-07-27 12:24:12 +0530

    Update SmallestNonRepresentableSum.java

--- week-2/SmallestNonRepresentableSum.java
+++ week-2/SmallestNonRepresentableSum.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class SmallestNonRepresentableSum {
-    
-}
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        System.out.println("Enter the sorted array");
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println(solution(array,n));
+        
+        }
+
+
+        public static int solution(int[] arr,int n){
+            
+        }
+    }
